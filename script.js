b21827c6f47078345c259bc642472a43e4372494
Gabriel Oliveira dos Santos <oliveirasgah@gmail.com>
2023-10-03 17:19:25 -0300

    Add events for buttons and command to clear screen

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ function divide(a, b) {
 function operate(number1, number2, operator) {
 
 }
+
+function addNumberScreen(number) {
+    const textScreen = document.querySelector('.screen__text');
+    const textContentScreen = textScreen.textContent;
+
+    if(textContentScreen.length < 8) {
+        textScreen.textContent = (textContentScreen === '0') ?
+            number : textScreen.textContent + number;
+    }
+}
+
+function executeCommand(command) {
+    if(command === 'C') {
+        const textScreen = document.querySelector('.screen__text');
+        textScreen.textContent = '0';
+    }
+}
+
+const buttons = document.querySelectorAll('.keyboard__button');
+
+buttons.forEach(button => button.addEventListener('click', e => {
+    const buttonText = e.target.textContent;
+
+    if(isNaN(buttonText)) {
+        executeCommand(buttonText)
+    } else {
+        addNumberScreen(buttonText);
+    }
+}));
